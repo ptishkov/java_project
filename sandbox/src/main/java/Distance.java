228622e3c4a896deee59af082afc3929737a3d0a
ptishkov <ptishkov@baltbet.ru>
2022-08-13 12:22:44 +0300

    Добавление конструктора в Point для удобства

--- sandbox/src/main/java/Distance.java
+++ sandbox/src/main/java/Distance.java
@@ -1,12 +1,8 @@
 public class Distance {
 	
 	public static void main(String [] args) {
-		Point a = new Point();
-		a.p1 = 10;
-		a.p2 = 15;
-		Point b = new Point();
-		b.p1 = 3;
-		b.p2 = 17;
+		Point a = new Point(10, 15);
+		Point b = new Point(3, 17);
 		System.out.println("Расстояние между точками a (" + a.p1 + "; " + a.p2 +
 				") и b (" + b.p1 + "; " + b.p2 + ") = " + distance(a,b));
 	}
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -3,4 +3,8 @@ public class Point {
     public double p1;
     public double p2;
 
+    public Point(double x, double y) {
+        p1 = x;
+        p2 = y;
+    }
 }
