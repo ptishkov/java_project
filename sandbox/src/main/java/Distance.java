9cf1daca5643ee5f8bb916279ac0eaa7dada500f
ptishkov <ptishkov@baltbet.ru>
2022-08-13 13:09:41 +0300

    Удаление функции и добавление метода distance в Point

--- sandbox/src/main/java/Distance.java
+++ sandbox/src/main/java/Distance.java
@@ -1,15 +1,10 @@
 public class Distance {
 	
 	public static void main(String [] args) {
-		Point a = new Point(10, 15);
-		Point b = new Point(3, 17);
+		Point a = new Point(7, 3);
+		Point b = new Point(15, 12);
 		System.out.println("Расстояние между точками a (" + a.p1 + "; " + a.p2 +
-				") и b (" + b.p1 + "; " + b.p2 + ") = " + distance(a,b));
+				") и b (" + b.p1 + "; " + b.p2 + ") = " + a.distance(a, b));
 	}
-
-	public static double distance(Point a, Point b) {
-		return Math.sqrt((a.p1 - b.p1) * (a.p1 - b.p1) + (a.p2 - b.p2) * (a.p2 - b.p2));
-	}
-
 
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -3,8 +3,13 @@ public class Point {
     public double p1;
     public double p2;
 
-    public Point(double x, double y) {
-        p1 = x;
-        p2 = y;
+    public Point(double p1, double p2) {
+        this.p1 = p1;
+        this.p2 = p2;
     }
+
+    public double distance(Point a, Point b) {
+        return Math.sqrt((a.p1 - b.p1) * (a.p1 - b.p1) + (a.p2 - b.p2) * (a.p2 - b.p2));
+    }
+
 }
