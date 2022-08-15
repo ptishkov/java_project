ed6ee2cad5378e4a3853065643df95054f0a1d3b
ptishkov <ptishkov@baltbet.ru>
2022-08-15 12:28:39 +0300

    Добавление тестов для проверки вычисления расстояния между точками

--- sandbox/src/test/java/task/PointTest.java
+++ sandbox/src/test/java/task/PointTest.java
@@ -1,12 +1,18 @@
 package task;
 
-import org.testng.annotations.Test;
+
+import org.testng.Assert;
 
 public class PointTest {
-    @Test
+    @org.testng.annotations.Test
     public void testDistance() {
         Point p1 = new Point(7, 3);
         Point p2 = new Point(15, 12);
-        assert p1.distance(p2) == 12;
+        Assert.assertEquals(p1.distance(p2), 12.041594578792296);
+
+        Point p3 = new Point(20, 40);
+        Point p4 = new Point(11, 22);
+        Assert.assertEquals(p3.distance(p4), 20.12461179749811);
+
     }
 }
