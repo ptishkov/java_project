e1c7428e652c26475665d292e9ebfce6aa010d64
ptishkov <ptishkov@baltbet.ru>
2022-09-08 21:22:48 +0300

    Добавление проверки списков после удаления группы и контакта

--- addressbook-web-tests/src/test/java/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
     private final String firstname;
     private final String lastname;
@@ -33,5 +35,40 @@ public class ContactData {
 
     public String getEmail() {
         return email;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (!Objects.equals(firstname, that.firstname)) return false;
+        if (!Objects.equals(lastname, that.lastname)) return false;
+        if (!Objects.equals(address, that.address)) return false;
+        if (!Objects.equals(mobileNumber, that.mobileNumber)) return false;
+        return Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (mobileNumber != null ? mobileNumber.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
     }
 }
