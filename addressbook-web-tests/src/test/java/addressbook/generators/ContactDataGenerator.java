f0c3081eb1948da3f3942fc70a677209e4059865
ptishkov <ptishkov@baltbet.ru>
2022-09-25 13:52:10 +0300

    Создание генератора контактов

---
+++ addressbook-web-tests/src/test/java/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,42 @@
+package addressbook.generators;
+import addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress(),
+                    contact.getHome(), contact.getMobile(), contact.getWork(),
+                    contact.getEmail(), contact.getEmail2(), contact.getEmail3()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
+                    .withLastname(String.format("lastname %s", i)).withAddress(String.format("address %s", i))
+                    .withHomePhone(String.format("111 %s", i)).withMobilePhone(String.format("2222 %s", i)).withWorkPhone(String.format("33333 %s", i))
+                    .withEmail(String.format("email@mail.ru %s", i)).withEmail2(String.format("email2@mail.ru %s", i))
+                    .withEmail3(String.format("email3@mail.ru %s", i)));
+        }
+        return contacts;
+    }
+}
