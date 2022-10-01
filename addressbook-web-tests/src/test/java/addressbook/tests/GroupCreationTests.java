ca9b218b24b171067b98a2c1c4ed149b064b6ac6
ptishkov <ptishkov@baltbet.ru>
2022-10-01 12:48:18 +0300

    Создание и загрузка тестовых данных группы в формате xml

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
@@ -2,12 +2,13 @@ package addressbook.tests;
 
 import addressbook.model.GroupData;
 import addressbook.model.Groups;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,15 +17,18 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    xstream.allowTypes(new Class[] {GroupData.class});
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
   @Test(dataProvider = "validGroups")
   public void testGroupCreation(GroupData group){
