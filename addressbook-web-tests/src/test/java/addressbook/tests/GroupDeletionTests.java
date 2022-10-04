1a7f7dd2ff9e6b99676ace5d47f082fc155b9971
ptishkov <ptishkov@baltbet.ru>
2022-10-04 17:53:47 +0300

    Добавление сравнения данных из БД для удаления групп

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
@@ -6,24 +6,24 @@ import org.testng.annotations.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
       app.group().create(new GroupData().withName("1234").withHeader("1234").withFooter("1234"));
     }
   }
   @Test
   public void testGroupDeletion(){
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
+    app.goTo().groupPage();
     app.group().delete(deletedGroup);
     assertThat(app.group().count(), equalTo(before.size() - 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(deletedGroup)));
   }
 }
