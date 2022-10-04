93bcdf4c5213320e8a0df3761514c2e97f048696
ptishkov <ptishkov@baltbet.ru>
2022-10-04 19:24:01 +0300

    Добавление проверок UI для модификации групп

--- addressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
@@ -1,9 +1,15 @@
 package addressbook.tests;
 
 import addressbook.appmanager.ApplicationManager;
+import addressbook.model.GroupData;
+import addressbook.model.Groups;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.util.stream.Collectors;
 
 public class TestBase {
 
@@ -19,6 +25,16 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            MatcherAssert.assertThat(uiGroups, CoreMatchers.equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
     }
 
 }
