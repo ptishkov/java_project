87ac45511277b8c5a8a595b7c862a8ad5a16e52e
ptishkov <ptishkov@baltbet.ru>
2022-10-04 19:01:38 +0300

    Добавление сравнения данных из БД для удаления контактов

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
@@ -3,29 +3,32 @@ package addressbook.tests;
 import addressbook.model.ContactData;
 import addressbook.model.Contacts;
 import org.testng.annotations.*;
+
+import java.io.File;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class ContactDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
+    if (app.db().contacts().size() == 0) {
     app.goTo().home();
-    if (app.contact().all().size() == 0) {
-      app.contact().create(new ContactData().withFirstname("Name").withLastname("LastName")
-              .withAddress("Saint-Petersburg").withHomePhone("+79215553322").withEmail("ptishkov@baltbet.ru"));
+    app.contact().create(new ContactData().withFirstname("Name").withLastname("LastName").withAddress("Saint-Petersburg")
+              .withHomePhone("+79215553322").withMobilePhone("+79111111123").withWorkPhone("+2951717")
+              .withEmail("ptishkov@baltbet.ru").withEmail2("ratatatata@gmail.com").withEmail3("vvpitun@mail.ru")
+              .withPhoto(new File("src\\test\\resources\\unknown.png")));
     }
   }
   @Test
   public void testContactDeletion() {
-    Set<ContactData> before = app.contact().all();
+    Set<ContactData> before = app.db().contacts();
     ContactData deletedContact = before.iterator().next();
     app.contact().delete(deletedContact);
     assertThat(app.contact().count(), equalTo(before.size() - 1));
-    Set<ContactData> after = app.contact().all();
+    Set<ContactData> after = app.db().contacts();
     assertThat(after, equalTo(((Contacts) before).without(deletedContact)));
   }
 
--- addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
@@ -15,7 +15,7 @@ public class ContactModificationTests extends TestBase {
     public void ensurePreconditions() {
         if (app.db().contacts().size() == 0) {
         app.goTo().home();
-            app.contact().create(new ContactData().withFirstname("Name").withLastname("LastName").withAddress("Saint-Petersburg")
+        app.contact().create(new ContactData().withFirstname("Name").withLastname("LastName").withAddress("Saint-Petersburg")
                     .withHomePhone("+79215553322").withMobilePhone("+79111111123").withWorkPhone("+2951717")
                     .withEmail("ptishkov@baltbet.ru").withEmail2("ratatatata@gmail.com").withEmail3("vvpitun@mail.ru")
                     .withPhoto(new File("src\\test\\resources\\unknown.png")));
