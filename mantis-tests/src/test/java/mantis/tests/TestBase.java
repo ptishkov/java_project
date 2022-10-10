5e7082f3b346634fe60128c17ae88bb47594192c
ptishkov <ptishkov@baltbet.ru>
2022-10-10 18:00:06 +0300

    Добавлен новый модуль для тестов для mantis

---
+++ mantis-tests/src/test/java/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package mantis.tests;
+
+import mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
