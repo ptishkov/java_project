0901cbe9b80624dc1393c76653432251d143f6dd
ptishkov <ptishkov@baltbet.ru>
2022-10-21 13:24:35 +0300

    Изменён тест для проверки Jenkins

--- sandbox/src/test/java/task/PointTest.java
+++ sandbox/src/test/java/task/PointTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 public class PointTest {
     @org.testng.annotations.Test
     public void testDistance1() {
-        Point p1 = new Point(7, 3);
+        Point p1 = new Point(3, 3);
         Point p2 = new Point(15, 12);
         Assert.assertEquals(p1.distance(p2), 12.041594578792296);
     }
